3896a0e8c515d996f2b7ff1eedc36e9a4e51f04c
Ercan Er <iosyazilim@gmail.com>
2018-11-26 22:56:05 +0300

    Prettier Code

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,60 +1,48 @@
-import { createStore } from 'redux';
+import { createStore } from "redux";
 
-
-const store = createStore((state = { count: 0}, action) => {
-    if(action.type === 'INCREMENT'){
-        return {
-            count: state.count + 1
-        }
-    }
-    if(action.type === 'DECREMENT'){
-        return {
-            count: state.count - 1
-        }
-    }
-    if(action.type === 'RESET'){
-        return {
-            count: 0
-        }
-    }
-     else {
-        return state;
-    }
-
+const store = createStore((state = { count: 0 }, action) => {
+  if (action.type === "INCREMENT") {
+    return {
+      count: state.count + 1
+    };
+  }
+  if (action.type === "DECREMENT") {
+    return {
+      count: state.count - 1
+    };
+  }
+  if (action.type === "RESET") {
+    return {
+      count: 0
+    };
+  } else {
+    return state;
+  }
 });
 
 console.log(store.getState());
 
 // I would like to increment count
 store.dispatch({
-    type: 'INCREMENT'
-
+  type: "INCREMENT"
 });
 store.dispatch({
-    type: 'INCREMENT'
-
+  type: "INCREMENT"
 });
 store.dispatch({
-    type: 'INCREMENT'
-
+  type: "INCREMENT"
 });
 store.dispatch({
-    type: 'INCREMENT'
-
+  type: "INCREMENT"
 });
 store.dispatch({
-    type: 'RESET'
-
+  type: "RESET"
 });
-store.dispatch(
-    {
-    type: 'DECREMENT'
-    }
-);
 store.dispatch({
-    type: 'DECREMENT'
-
+  type: "DECREMENT"
 });
-
+store.dispatch({
+  type: "DECREMENT"
+});
 
 console.log(store.getState());
