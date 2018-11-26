7396f6e3459d8cffb974160347a110afbfe6cd67
Ercan Er <iosyazilim@gmail.com>
2018-11-26 17:39:27 +0300

    Added Redux
    getState and dispatch example

---
+++ src/playground/redux-101.js
@@ -0,0 +1,60 @@
+import { createStore } from 'redux';
+
+
+const store = createStore((state = { count: 0}, action) => {
+    if(action.type === 'INCREMENT'){
+        return {
+            count: state.count + 1
+        }
+    }
+    if(action.type === 'DECREMENT'){
+        return {
+            count: state.count - 1
+        }
+    }
+    if(action.type === 'RESET'){
+        return {
+            count: 0
+        }
+    }
+     else {
+        return state;
+    }
+
+});
+
+console.log(store.getState());
+
+// I would like to increment count
+store.dispatch({
+    type: 'INCREMENT'
+
+});
+store.dispatch({
+    type: 'INCREMENT'
+
+});
+store.dispatch({
+    type: 'INCREMENT'
+
+});
+store.dispatch({
+    type: 'INCREMENT'
+
+});
+store.dispatch({
+    type: 'RESET'
+
+});
+store.dispatch(
+    {
+    type: 'DECREMENT'
+    }
+);
+store.dispatch({
+    type: 'DECREMENT'
+
+});
+
+
+console.log(store.getState());
