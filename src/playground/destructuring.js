5985e8627e39d34a240ae638cbdb5544a31aa8f4
Ercan Er <iosyazilim@gmail.com>
2018-11-27 17:08:48 +0300

    Redux working

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,6 @@
+//
+//Object Destructing
+//
 const person = {
     name: 'Ercan',
     age: 29,
@@ -7,4 +10,44 @@ const person = {
     }
 };
 
-console.log(`${person.name} is ${person.age}`);
+// const {name : firstName ='Default Name', age} = person;
+
+// console.log(`${firstName} is ${age}`);
+
+// const {city, temp:tempeture} = person.location;
+
+// if(tempeture && city ) {
+
+//     console.log(`${city} is temp : ${tempeture}`);
+
+// }
+
+// const book = {
+//     title: "Ego is Enemy",
+//     author: "Ryan Holiday",
+//     publisher: {
+//         name: "Penguin"
+//     }
+// };
+
+// const {title, author} = book;
+
+// const {name: publisherName = 'Self-Published'} = book.publisher;
+
+// console.log(`${publisherName}`);
+
+//
+//Array Destructing
+//
+
+const address = ["Koru Mah.","Çankaya", , "06876"];
+
+const [street, state, city='Samsun', zip] = address;
+
+console.log(`My Adress ${street} ${state} ${city}`);
+
+const item = ["Coffe (hot)", "$2.00","$2.50" ,"$3.00"];
+
+const [coffee, priceShort, priceTall, priceBig] = item;
+
+console.log(`A medium ${coffee} costs ${priceTall}`);
--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,63 +1,65 @@
 import { createStore } from "redux";
 
-const store = createStore((state = { count: 0 }, action) => {
-  if (action.type === "INCREMENT") {
-    const incrementBy =
-      typeof action.incrementBy === "number" ? action.incrementBy : 1;
-    return {
-      count: state.count + incrementBy
-    };
-  }
-  if (action.type === "DECREMENT") {
-    const decrementBy =
-      typeof action.decrementBy === "number" ? action.decrementBy : 1;
-    return {
-      count: state.count - decrementBy
-    };
-  }
-  if (action.type === "RESET") {
-    return {
-      count: 0
-    };
-  }
-  if (action.type === "SET") {
-    return {
-      count: action.count
-    };
-  } else {
-    return state;
-  }
+const incrementCount = ({incrementBy = 1} = {}) => ({
+    type:'INCREMENT',
+    incrementBy
 });
+
+const decrementCount = ({ decrementBy = 1} = {}) =>({
+    type:'DECREMENT',
+    decrementBy
+}); 
+
+const setCount = ({ count }) => ({
+    type:'SET',
+    count
+});
+
+const resetCount = () =>({
+    type: 'RESET'
+});
+
+//Reducer
+//1. Reducer are pure functions
+//2. Never change state or action
+
+const countReducer = (state = { count: 0 }, action) => {
+    if (action.type === "INCREMENT") {
+      return {
+        count: state.count + action.incrementBy
+      };
+    }
+    if (action.type === "DECREMENT") {
+      return {
+        count: state.count - action.decrementBy
+      };
+    }
+    if (action.type === "RESET") {
+      return {
+        count: 0
+      };
+    }
+    if (action.type === "SET") {
+      return {
+        count: action.count
+      };
+    } else {
+      return state;
+    }
+  };
+
+const store = createStore(countReducer);
 
 const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
 });
 
-// I would like to increment count
-store.dispatch({
-  type: "INCREMENT",
-  incrementBy: 5
-});
-store.dispatch({
-  type: "INCREMENT"
-});
-store.dispatch({
-  type: "INCREMENT"
-});
-store.dispatch({
-  type: "INCREMENT"
-});
-store.dispatch({
-  type: "RESET"
-});
-store.dispatch({
-  type: "DECREMENT"
-});
-store.dispatch({
-  type: "DECREMENT",
-  decrementBy: 10
-});
-store.dispatch({
-  type: "SET",
-  count: 101
-});
+store.dispatch(incrementCount( {incrementBy: 5} ));
+
+store.dispatch(incrementCount());
+
+store.dispatch(resetCount());
+
+store.dispatch(decrementCount({decrementBy: 49}));
+
+store.dispatch(setCount({count: 101}));
