1d13be7eda19b88763f6d4b7b25ca7bc35335aa3
Ercan Er <iosyazilim@gmail.com>
2018-12-03 01:43:23 +0300

    Redux
    Redux integration my budget project

--- src/app.js
+++ src/app.js
@@ -3,5 +3,20 @@ import ReactDOM from "react-dom";
 import "./styles/style.scss";
 import "normalize.css/normalize.css";
 import AppRouter from "./routers/AppRouter";
+import configureStore from './store/configureStore';
+import {addExpense} from './actions/expenses';
+import {setTextFilter} from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+
+const store = configureStore();
+
+store.dispatch(addExpense({description: 'Water Bill'}));
+store.dispatch(addExpense({description: 'Gas Bill'}));
+store.dispatch(setTextFilter('s'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
 
 ReactDOM.render(<AppRouter />, document.getElementById("app"));
