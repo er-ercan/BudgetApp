c7ca9c12b88db316712606ab11be27c267af7c49
Ercan Er <iosyazilim@gmail.com>
2018-12-10 00:46:42 +0300

    Minor Change

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import {connect} from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
-const ExpenseList = (props) => (
-    <div>
-        <h1>Expense List</h1>
-        {props.filters.text}
-        {props.expenses.length}
-    </div>
+const ExpenseList = props => (
+  <div>
+    <h1>Expense List</h1>
+    {props.filters.text}
+    {props.expenses.length}
+  </div>
 );
 
-
-const mapStateToProps = (state) => {
-    return {
-        expenses: state.expenses,
-        filters: state.filters
-    }
-};  
+const mapStateToProps = state => {
+  return {
+    expenses: state.expenses,
+    filters: state.filters
+  };
+};
 
 export default connect(mapStateToProps)(ExpenseList);
